178b1429de0471b366534d8e1d7c29f98172a266
Matthew Freire <matthew@Matthews-MacBook-Pro.local>
2018-02-11 23:26:01 +0200

    Models

--- posts/models.py
+++ posts/models.py
@@ -2,8 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
+
 class Post(models.Model):
 	title = models.CharField(max_length=120)
+	description = models.TextField()
+	author = models.OneToOneField('Author')
+	image = models.ImageField()
+	slug = models.SlugField()
 
 	def __str__(self):
 		return self.title
+
+
+class Author(models.Model):
+	user = models.ForeignKey(User)
+	email = models.EmailField()
+	cellphone_num = models.IntegerField()# "+27"
+
+	def __str__(self):
+		return self.user.username
